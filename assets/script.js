3704521eeeea7794aaa44a36fa95e3bed28cd0da
RyanEbanks <ryankebanks@gmail.com>
2022-11-11 12:15:16 -0500

    Added more JavaScript along with HTML Changes

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,29 @@
 //Creating Conditions for the different topics
 
-var topics=["HTML", "CSS", "Git", "JavaScript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if(topic === 'HTML'){
-    console.log("Lets Study HTML!");
-} else if(topic === 'CSS'){
-    console.log("Lets Study CSS!");
-}else if(topic === 'Git'){
-    console.log("Lets Study Git!");
-}else if(topic === 'JavaScript'){
-    console.log("Lets Study JavaScript!");
-} else {
-    console.log("Please try again!");
-}
-*/
-for(var i = 0; i < topics.length; i++){
-    console.log("Lets Study", topics[i], "!");
+function listTopics(){
+    for(var i = 0; i < topics.length; i++){
+        console.log(topics[i]);
+    }
 }
 
+function selectTopic(){
+    if(randomTopic === 'HTML'){
+        console.log("Lets Study HTML!");
+    } else if(randomTopic === 'CSS'){
+        console.log("Lets Study CSS!");
+    }else if(randomTopic === 'Git'){
+        console.log("Lets Study Git!");
+    }else if(randomTopic === 'JavaScript'){
+        console.log("Lets Study JavaScript!");
+    } else {
+        console.log("Please try again!");
+    }
+}
+
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
