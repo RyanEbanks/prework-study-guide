e660a6a1afda166f7684b69f5dfe48647df32cf9
RyanEbanks <ryankebanks@gmail.com>
2022-11-11 11:21:06 -0500

    Added a Java Script file to the project and edited HTML

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//Creating Conditions for the different topics
+
+var topics=["HTML", "CSS", "Git", "JavaScript"];
+
+/*
+if(topic === 'HTML'){
+    console.log("Lets Study HTML!");
+} else if(topic === 'CSS'){
+    console.log("Lets Study CSS!");
+}else if(topic === 'Git'){
+    console.log("Lets Study Git!");
+}else if(topic === 'JavaScript'){
+    console.log("Lets Study JavaScript!");
+} else {
+    console.log("Please try again!");
+}
+*/
+for(var i = 0; i < topics.length; i++){
+    console.log("Lets Study", topics[i], "!");
+}
+
